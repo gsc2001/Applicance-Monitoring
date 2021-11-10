8d805fefa38907d428bc974d5cd232a272197eda
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2021-11-10 18:38:40 +0530

    Add om2m, subscription

--- core/main/server/app/main.py
+++ core/main/server/app/main.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Body
 from .utils.influxdb import connect_db, disconnect_db, get_database
 from influxdb_client.client.write_api import WriteApi
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -10,7 +11,7 @@ app.add_event_handler("startup", connect_db)
 app.add_event_handler("shutdown", disconnect_db)
 
 org = "gurkiratsingh2001@gmail.com"
-bucket = "gurkiratsingh2001's Bucket"
+bucket = "esw"
 
 
 @app.post("/items/{item_id}")
@@ -18,3 +19,9 @@ def read_item(item_id: int, q: Optional[str] = None, db: WriteApi = Depends(get_
     data = f"mem,host=host1 used_percent={item_id}"
     db.write(bucket, org, data)
     return {"item_id": item_id, "q": q}
+
+
+@app.post('/test')
+async def om2m_callback(body=Body(...)):
+
+    return "Success"

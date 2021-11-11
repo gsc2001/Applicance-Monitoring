3ffc87a5f52cd05fd2be74f9574d8e6a923014a5
Anmol Agarwal <anmolagarwal4453@gmail.com>
2021-11-11 09:49:08 +0530

    improved encryp also

--- crypto_explore/check_hashing/decrypt.py
+++ crypto_explore/check_hashing/decrypt.py
@@ -1,15 +1,31 @@
+import sys
+
 XOR_KEY = "PQRST";
-INPUT="acagaffjjd"
-
-
+INPUT=input()
+print(len(INPUT))
+RAND_MASTER=12
+RAND_NUM=0
+print("Input is ",INPUT)
 def decrypt_string(s):
     xor_len=len(XOR_KEY)
     ans=""
+    global RAND_NUM
     idx=-1
     for ch in INPUT:
+        # print(ch)
         idx+=1
+        RAND_NUM+=1
         idx%=xor_len
-        ans+=chr(ord(ch)^ord(XOR_KEY[idx]))
+        # print(ord(XOR_KEY[idx]))
+        # print(ord(ch))
+        num=ord(ch)^ord(XOR_KEY[idx])
+        print(num)
+        num-=RAND_NUM
+        left=num
+        if left<0:
+            num=256-abs(left)
+        ans+=chr(num)
+        # print("******")
     return ans
 
 final_ans=decrypt_string(INPUT)
--- crypto_explore/check_hashing/xor_cipher.cpp
+++ crypto_explore/check_hashing/xor_cipher.cpp
@@ -35,7 +35,8 @@ LL POW(LL x, LL y, LL mod_give)
 }
 
 string XOR_KEY = "PQRST";
-int rand_num = 12;
+int rand_master = 12;
+int rand_num = 0;
 string encrypt_string(string s)
 {
     int idx = -1;
@@ -43,9 +44,16 @@ string encrypt_string(string s)
     string ans = "";
     for (auto &x : s)
     {
+        rand_num += 1;
+        rand_num %= rand_master;
         idx += 1;
         idx %= key_len;
-        ans += x ^ XOR_KEY[idx];
+        // debug(int(XOR_KEY[idx]));
+        int want=((rand_num + x) % 256) ^ XOR_KEY[idx];
+        // cout<<((rand_num + x) % 256)<<" ^ "<<int(XOR_KEY[idx])<<"="<<want<<endl;
+        // debug(want);
+        // cout<<x<<" "<<want<<endl;
+        ans += char(want);
     }
     return ans;
 }
@@ -54,9 +62,11 @@ int main()
     fastinput;
     LL n, i, j, k, t, temp, tc;
     string s;
-    cin >> s;
+    s="1234567890";
+    // cin >> s;
     string encrypted_version = encrypt_string(s);
-    debug(encrypted_version);
-    debug(encrypt_string(encrypted_version));
+    // debug(encrypted_version);
+    // debug(encrypt_string(encrypted_version));
+    cout << encrypted_version;
     return 0;
 }

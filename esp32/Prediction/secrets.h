88975d760d1d9c913cc8af37a162517fbafcb89f
Anmol Agarwal <anmolagarwal4453@gmail.com>
2021-12-07 12:41:16 +0530

    added 2 new folders, not sure why these were unpushed

---
+++ esp32/Prediction/secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define SSID "Agarwal Wifi 3901_EXT"
+#define PASS "air53482"
+
+String OM2M_SERVER = "http://192.168.1.7:8080/~/in-cse/in-name/Team-19/Prediction/Data/";
+
+#endif

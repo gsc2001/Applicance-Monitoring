54828dfe87945c67b305c73e5b13af1a72d99563
Gurkirat Singh <gurkiratsingh2001@gmail.com>
2021-12-07 23:06:59 +0530

    Revert back to old encryption 2

--- core/main/server/app/utils/decrypt.py
+++ core/main/server/app/utils/decrypt.py
@@ -1,6 +1,3 @@
-from Crypto.Cipher import AES
-import binascii
-
 XOR_KEY = "AB12:>"
 
 RAND_MASTER = 12
@@ -18,20 +15,6 @@ def populate_stuff():
 
 
 populate_stuff()
-
-# KEY = "abcdefghijklmnop"
-# cipher = AES.new(KEY, AES.MODE_ECB)
-#
-#
-# def decrypt_string(s):
-#     s = binascii.unhexlify(s)
-#     out = ""
-#     for i in range(0, len(s), 16):
-#         s2 = cipher.decrypt(s[i : i + 16])[:-1]
-#         out += s2.decode("utf-8")
-#
-#     out = out.strip()
-#     return out
 
 
 def decrypt_string(s):
@@ -52,7 +35,3 @@ def decrypt_string(s):
             num = 64 - abs(left)
         ans += ascii_to_char[num]
     return ans
-
-# INPUT=input()
-# final_ans = decrypt_string(INPUT)
-# print(final_ans)

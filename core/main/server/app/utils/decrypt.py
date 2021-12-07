cdc4686edcf3f389582c8996cdad345b2cc596d7
tichnas <sanchit.arora.2002@gmail.com>
2021-12-08 00:04:35 +0530

    Encryption & decryption

--- core/main/server/app/utils/decrypt.py
+++ core/main/server/app/utils/decrypt.py
@@ -1,57 +1,57 @@
 from Crypto.Cipher import AES
 import binascii
 
-XOR_KEY = "AB12:>"
+# XOR_KEY = "AB12:>"
 
-RAND_MASTER = 12
-RAND_NUM = 0
+# RAND_MASTER = 12
+# RAND_NUM = 0
 
-ascii_to_char = dict()
-char_to_ascii = dict()
-
-
-def populate_stuff():
-    for i in range(64):
-        ch = chr(i + 44)
-        char_to_ascii[ch] = i
-        ascii_to_char[i] = ch
+# ascii_to_char = dict()
+# char_to_ascii = dict()
 
 
-populate_stuff()
+# def populate_stuff():
+#     for i in range(64):
+#         ch = chr(i + 44)
+#         char_to_ascii[ch] = i
+#         ascii_to_char[i] = ch
+
+
+# populate_stuff()
 
 KEY = "abcdefghijklmnop"
 cipher = AES.new(KEY, AES.MODE_ECB)
 
 
-# def decrypt_string(s):
-#     s = binascii.unhexlify(s)
-#     out = ""
-#     for i in range(0, len(s), 16):
-#         s2 = cipher.decrypt(s[i : i + 16])[:-1]
-#         out += s2.decode("utf-8")
-
-#     out = out.strip()
-#     return out
-
-
 def decrypt_string(s):
-    xor_len = len(XOR_KEY)
-    ans = ""
-    global RAND_NUM
-    idx = -1
-    for ch in s:
-        idx += 1
-        # RAND_NUM += 1
-        # RAND_NUM %= RAND_MASTER
-        idx %= xor_len
-        num = char_to_ascii[ch] ^ char_to_ascii[XOR_KEY[idx]]
-        # print(num)
-        num -= RAND_NUM
-        left = num
-        if left < 0:
-            num = 64 - abs(left)
-        ans += ascii_to_char[num]
-    return ans
+    s = binascii.unhexlify(s)
+    out = ""
+    for i in range(0, len(s), 16):
+        s2 = cipher.decrypt(s[i : i + 16])
+        out += s2.decode("utf-8")
+
+    out = out.strip()
+    return out
+
+
+# def decrypt_string(s):
+#     xor_len = len(XOR_KEY)
+#     ans = ""
+#     global RAND_NUM
+#     idx = -1
+#     for ch in s:
+#         idx += 1
+#         # RAND_NUM += 1
+#         # RAND_NUM %= RAND_MASTER
+#         idx %= xor_len
+#         num = char_to_ascii[ch] ^ char_to_ascii[XOR_KEY[idx]]
+#         # print(num)
+#         num -= RAND_NUM
+#         left = num
+#         if left < 0:
+#             num = 64 - abs(left)
+#         ans += ascii_to_char[num]
+#     return ans
 
 
 # INPUT=input()
--- esp32/Prediction/OM2MServer/OM2M.cpp
+++ esp32/Prediction/OM2MServer/OM2M.cpp
@@ -5,44 +5,70 @@
 #include "../secrets.h"
 #include "../config.h"
 
-String XOR_KEY = "AB12:>";
-int rand_master = 12;
-int rand_num = 0;
+// String XOR_KEY = "AB12:>";
+// int rand_master = 12;
+// int rand_num = 0;
 
 // String encrypt_string(String s)
 // {
-//     return s;
-//     // int idx = -1;
-//     // int key_len = XOR_KEY.length();
-//     // String ans = "";
-//     // for (auto &x : s)
-//     // {
-//     //     rand_num += 1;
-//     //     rand_num %= rand_master;
-//     //     idx += 1;
-//     //     idx %= key_len;
-//     //     int want = ((rand_num + (x - 44)) % 64) ^ (XOR_KEY[idx] - 44);
-//     //     ans += want + 44;
-//     // }
-//     // return ans;
+//     int idx = -1;
+//     int key_len = XOR_KEY.length();
+//     String ans = "";
+//     for (int i = 0; i < s.length(); i++)
+//     {
+//         char x = s.charAt(i);
+//         // Serial.println(x);
+//         // delay(1000);
+//         idx += 1;
+//         idx %= key_len;
+//         int want = ((rand_num + (x - 44)) % 64) ^ (XOR_KEY[idx] - 44);
+//         ans += char(want + 44);
+//     }
+//     return ans;
 // }
+
+char *key = "abcdefghijklmnop";
+unsigned char output[100];
+char input[100];
+
+String encrypt_block(String inp)
+{
+
+    inp.toCharArray(input, inp.length() + 1);
+
+    mbedtls_aes_context aes;
+    mbedtls_aes_init(&aes);
+    mbedtls_aes_setkey_enc(&aes, (const unsigned char *)key, strlen(key) * 8);
+    mbedtls_aes_crypt_ecb(&aes, MBEDTLS_AES_ENCRYPT, (const unsigned char *)input, output);
+    mbedtls_aes_free(&aes);
+    String out = "";
+    for (int i = 0; i < 16; i++)
+    {
+        char str[3];
+        sprintf(str, "%02x", (int)output[i]);
+        out += String(str);
+    }
+
+    // Serial.println(out);
+    return out;
+}
 
 String encrypt_string(String s)
 {
-    int idx = -1;
-    int key_len = XOR_KEY.length();
-    String ans = "";
-    for (int i = 0; i < s.length(); i++)
+    while (s.length() % 16 != 0)
     {
-        char x = s.charAt(i);
-        // Serial.println(x);
-        // delay(1000);
-        idx += 1;
-        idx %= key_len;
-        int want = ((rand_num + (x - 44)) % 64) ^ (XOR_KEY[idx] - 44);
-        ans += char(want + 44);
+        s += " ";
     }
-    return ans;
+
+    String encrypted = "";
+    for (int i = 0; i < s.length(); i += 16)
+    {
+        encrypted += encrypt_block(s.substring(i, i + 16));
+    }
+
+    // Serial.println("Encrypted");
+    // Serial.println(encrypted);
+    return encrypted;
 }
 
 int OM2M::init(int delay)
